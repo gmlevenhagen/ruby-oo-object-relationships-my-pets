ea80020f3e06f826db5c94b3a28d57f6fbf53b48
gmlevenhagen <garettlevenhagen@gmail.com>
2020-10-23 11:13:03 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+
+  def say_species(species)
+    puts "I am a #{species}."
+  end
+
+  
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
